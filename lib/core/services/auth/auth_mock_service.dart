470d9e2954ffba2d51e37ad11767599b3a18e743
Wisley César <wisleycesar32@gmail.com>
2025-02-14 10:24:53 -0300

    FEAT: Criando um usaurio padrão

--- lib/core/services/auth/auth_mock_service.dart
+++ lib/core/services/auth/auth_mock_service.dart
@@ -6,13 +6,21 @@ import 'package:chat/core/models/chat_user.dart';
 import 'package:chat/core/services/auth/auth_services.dart';
 
 class AuthMockService implements AuthServices {
-  static Map<String, ChatUser> _users = {};
+  static final _defaultUser = ChatUser(
+    id: '1',
+    name: 'Teste',
+    email: 'Teste@',
+    imageURL: 'assets/images/avatar.png',
+  );
+  static Map<String, ChatUser> _users = {
+    _defaultUser.email: _defaultUser,
+  };
   static ChatUser? _currentUser;
   static MultiStreamController<ChatUser?>? _controller;
 
   static final _userStream = Stream<ChatUser?>.multi((controller) {
     _controller = controller;
-    _updateUser(null);
+    _updateUser(_defaultUser);
   });
 
   ChatUser? get currentUser {
@@ -35,7 +43,7 @@ class AuthMockService implements AuthServices {
       id: Random().nextDouble().toString(),
       name: name,
       email: email,
-      imageURL: image?.path ?? '/assets/images/...',
+      imageURL: image?.path ?? 'assets/images/avatar.png',
     );
     _users.putIfAbsent(email, () => newUser);
     print(newUser.id);
