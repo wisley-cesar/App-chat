314707ddb1cf4b6003a2995b9bb6cd1c72276ec0
Wisley César <wisleycesar32@gmail.com>
2025-02-18 09:46:43 -0300

    FEAT: Criando a tela do chat

--- lib/core/services/auth/auth_mock_service.dart
+++ lib/core/services/auth/auth_mock_service.dart
@@ -7,9 +7,9 @@ import 'package:chat/core/services/auth/auth_service.dart';
 
 class AuthMockService implements AuthService {
   static final _defaultUser = ChatUser(
-    id: '1',
-    name: 'Teste',
-    email: 'Teste@',
+    id: '123',
+    name: 'Wisley',
+    email: 'wisley@gmail.com.br',
     imageURL: 'assets/images/avatar.png',
   );
   static Map<String, ChatUser> _users = {
