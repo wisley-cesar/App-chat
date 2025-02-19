eabb84d6ba6f78ab9396f485311d7a8cf88245cc
Wisley César <wisleycesar32@gmail.com>
2025-02-19 09:46:28 -0300

    FEAT: Criando um MultiProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:chat/core/services/notification/push_notification_service.dart';
 import 'package:chat/pages/auth_or_app_page.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,26 +12,31 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        textTheme: TextTheme(
-          displayLarge: TextStyle(
-              fontSize: 32, fontWeight: FontWeight.bold, color: Colors.blue),
-          displayMedium: TextStyle(
-              fontSize: 28, fontWeight: FontWeight.w600, color: Colors.white),
-          bodyLarge: TextStyle(fontSize: 18, color: Colors.black87),
-          bodyMedium: TextStyle(fontSize: 16, color: Colors.black54),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => PushNotificationService()),
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          textTheme: TextTheme(
+            displayLarge: TextStyle(
+                fontSize: 32, fontWeight: FontWeight.bold, color: Colors.blue),
+            displayMedium: TextStyle(
+                fontSize: 28, fontWeight: FontWeight.w600, color: Colors.white),
+            bodyLarge: TextStyle(fontSize: 18, color: Colors.black87),
+            bodyMedium: TextStyle(fontSize: 16, color: Colors.black54),
+          ),
+          colorScheme: ColorScheme.fromSeed(
+            seedColor: Colors.blue,
+            primary: Colors.blue,
+            secondary: Colors.white,
+          ),
+          useMaterial3: true,
         ),
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.blue,
-          primary: Colors.blue,
-          secondary: Colors.white,
-        ),
-        useMaterial3: true,
+        home: AuthOrAppPage(),
+        debugShowCheckedModeBanner: false,
       ),
-      home: AuthOrAppPage(),
-      debugShowCheckedModeBanner: false,
     );
   }
 }
